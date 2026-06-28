da1f0a0551f25150ea11b37fa34d9210594b1571
HuiqingPan <157884227+HuiqingPan@users.noreply.github.com>
2026-06-28 17:11:27 +0800

    Update config_baseline.py

--- arguments/config_baseline.py
+++ arguments/config_baseline.py
@@ -1,1 +1,11 @@
+from base.base import Base
 
+
+class Config_read(Base):
+    def __init__(self):
+        self.feature_dict = self.load_dict('baseline_dict.xlsx')
+        self.total_columns = self.get_structured_names(self.feature_dict, type_name=['colocolumns'])  # 所有结构化数据的列名，手动输入type_name则指定类型
+        self.categorical_columns = self.get_structured_names(self.feature_dict, type_name=['categorical'])
+        self.continuous_columns = self.get_structured_names(self.feature_dict, type_name=['continuous'])
+
+data = Config_read()
